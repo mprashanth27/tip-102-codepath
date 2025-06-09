28da499776deb769de3e268d5c5e740c845e3621
m.prashanth <prashanthmangena@gmail.com>
2025-06-09 13:18:56 -0700

    add file for rough

---
+++ cp_rough.py
@@ -0,0 +1,8 @@
+s = input("Enter problem name:") # Problem 1: Reverse Sentence
+s = s.lower()
+s_list = s.split()
+print(s_list)
+'''
+res= "_".join(s.split())
+res = "p_" + res + ".py"
+print(res) # p_reverse_sentence.py'''
