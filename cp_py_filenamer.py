3cb6fa88bc6610a7a0874d7ddba1d64daef21088
m.prashanth <prashanthmangena@gmail.com>
2025-06-09 17:52:43 -0700

    add cp_py_filenamer_v2

--- cp_py_filenamer.py
+++ cp_py_filenamer.py
@@ -1,5 +1,10 @@
-s = input("Enter problem name:") # Problem 1: Reverse Sentence
+'''Takes problem heading and generates a .py file name'''
+
+s = input("Enter problem name:") # i/p = Problem 1: Reverse Sentence
 s = s.lower()
-res= "_".join(s.split())
-res = "p_" + res + ".py"
-print(res) # p_reverse_sentence.py
+s_list = s.split() # ['problem', '1:', 'reverse', 'sentence']
+#print(s_list)
+p_num = s_list[1][0]
+p_name = "_".join(s_list[2:])
+file_name = "p" + p_num + "_" + p_name + ".py"
+print(file_name) # o/p = p1_reverse_sentence.py
