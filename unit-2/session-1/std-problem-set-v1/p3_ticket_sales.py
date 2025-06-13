093f01e09f6e16eb825a48aac801d24948c9f35c
m.prashanth <prashanthmangena@gmail.com>
2025-06-13 10:46:46 -0700

    add my p3 in U2S1StdV1

--- unit-2/session-1/std-problem-set-v1/p3_ticket_sales.py
+++ unit-2/session-1/std-problem-set-v1/p3_ticket_sales.py
@@ -1,14 +1,17 @@
-#U:
-# input: dict
-#output: sum of values (total number of tickets)
+'''
+U:
+i/p= ticket_sales: dict
+o/p= total_sales: int
 
-#edge cases:
-#empty dict
+#edge cases
+1. non-int val in ticket_sales dict
+2. empty ticket_sales dict {}
 
-
+P:
+return sum(dict.values)
+'''
 def total_sales(ticket_sales):
     return sum(ticket_sales.values())
-
 
 ticket_sales = {"Friday": 200, "Saturday": 1000, "Sunday": 800, "3-Day Pass": 2500}
 
