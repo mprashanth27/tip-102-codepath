78d614f87313a27489344011de1a45ae21db9b3c
m.prashanth <prashanthmangena@gmail.com>
2025-06-22 18:10:55 -0700

    add my U3S1StdV1 p4 two-pointer code attempt

--- unit-3/session-1/std-problem-set-v1/p4_engagement_boost.py
+++ unit-3/session-1/std-problem-set-v1/p4_engagement_boost.py
@@ -4,36 +4,65 @@ U-nderstand:
 i/p = engagement rates: list[int]; sorted in non-decreasing order
 o/p = list[int]; an array of the squares of each number sorted in non-decreasing order
 
-#Constraints:
+#Constraints: Modify the solution below to use the two-pointer technique
 
 #Edge cases
 
 
 P-lan:
+p1 : break list in to 2 halfs by length [WON'T WORK! as we would still need to sort it] 
+eg: [-4, -1, 0, 3, 10]
+[-4, -1, 0, 3, 10] > [-4, -1, 0] [ 3, 10]
+squared: l1 = [16, 1, 0], l2 = [ 9, 100]
+now use 2 same direction ptrs to merge them - l1 ptr, l2 ptr
+l1[0]<l2[0]
+res.append(l1[0]) 
+else res.append(l2[0]) which would append [9] but there is 0 in l1 which will be out of place until you sort it.
+So, need to break list in to halfs at 0 or 1st +ve num.
+
+p2 : break list in to halfs at 0 or 1st +ve num.
+
 
 
 I-mplement:
 '''
-#CodePath initial code
+#My two-pointer code
+# NOTE:THIS CODE WAS WRITTEN ASSUMING THERE WILL BE -VE NUMS IN THE I/P LIST, 
+# NEED TO VERIFY IF IT WORKS IF THEY ARE ONLY +VE NUMS IN THE I/P LIST
+
 def engagement_boost(engagements):
     squared_engagements = [] # output arr
     
-    # iterate thru i/p arr, square them & add to o/p arr
-    for i in range(len(engagements)):
-        squared_engagement = engagements[i] * engagements[i]
-        squared_engagements.append((squared_engagement, i))
+    def square_and_add_engagement(engagement_index): #helper func
+        squared_engagement = engagements[engagement_index] * engagements[engagement_index]
+        squared_engagements.append(squared_engagement)
     
-    squared_engagements.sort(reverse=True) # sort output arr in ascending order
-    #print(squared_engagements) #[(100, 4), (16, 0), (9, 3), (1, 1), (0, 2)]
-    
-    result = [0] * len(engagements)
-    position = len(engagements) - 1
-    
-    for square, original_index in squared_engagements:
-        result[position] = square
-        position -= 1
-    
-    return result
+  
+    if engagements[0] < 0:
+        # iterate thru i/p arr to find pivot i.e., 0 or 1st +ve num.
+        for i in range(len(engagements)):
+            if engagements[i] >= 0:
+                pivot = i
+                break
+        
+        list1_ptr = pivot # to move left from pivot
+        list2_ptr = pivot + 1 # to move right from pivot
+        while list1_ptr >= 0 and list2_ptr < len(engagements):
+            if engagements[list1_ptr] <= engagements[list1_ptr]:
+                square_and_add_engagement(list1_ptr)
+                list1_ptr -= 1
+            else:
+                square_and_add_engagement(list2_ptr)
+                list2_ptr += 1
 
-print(engagement_boost([-4, -1, 0, 3, 10]))
+        while list1_ptr >= 0: # add if any nums remaining in list1
+            square_and_add_engagement(list1_ptr)
+            list1_ptr -= 1
+        while list2_ptr < len(engagements): # add if any nums remaining in list2
+            square_and_add_engagement(list2_ptr)
+            list2_ptr += 1
+    
+    return squared_engagements
+
+print(engagement_boost([-4, -1, 0, 3, 10])) # o/p = [0, 1, 16, 9, 100] FAIL, CHECK LATER!
 #print(engagement_boost([-7, -3, 2, 3, 11]))
