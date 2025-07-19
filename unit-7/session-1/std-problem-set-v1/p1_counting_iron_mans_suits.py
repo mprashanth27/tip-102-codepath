3e158cfd4a9727b9a7103bef38b36eb3a6397fe5
m.prashanth <prashanthmangena@gmail.com>
2025-07-18 17:49:18 -0700

    add analysis in U7S1StdV1 p1

--- unit-7/session-1/std-problem-set-v1/p1_counting_iron_mans_suits.py
+++ unit-7/session-1/std-problem-set-v1/p1_counting_iron_mans_suits.py
@@ -4,7 +4,8 @@ U-nderstand: retrun #suits
 i/p = suits: list[str]
 o/p = count: int
 
-#Constraints: Implement the solution iteratively without the use of the len() function
+#Constraints: 
+1. Implement the solution iteratively without the use of the len() function
 
 #Edge cases
 [] > 0 #Pass
@@ -17,7 +18,7 @@ P-lan:
 
 I-mplement:
 '''
-
+# 1.Implement the solution iteratively without the use of the len() function.
 def count_suits_iterative(suits):
     # T = O(n), M = O(1)
     count = 0
@@ -26,14 +27,14 @@ def count_suits_iterative(suits):
         count += 1
     return count
 
-
+# 2.Implement the solution recursively.
 def count_suits_recursive(suits):
     # T = O(n), M = O(n) ~ due to recursion depth
     recursive_count = 0
     def recursion(suits):
         nonlocal recursive_count
 
-        if not suits:
+        if not suits: # base case
             return recursive_count
         
         suits.pop()
@@ -49,3 +50,9 @@ print(count_suits_recursive([]))
 
 print(count_suits_iterative(["Mark I"]))
 print(count_suits_recursive(["Mark I"]))
+
+# 3. Discuss: what are the similarities between the two solutions? What are the differences?
+'''
+main logic is same i.e., check suits is not empty > pop > increment the count.
+inorder to iterate while is used in #1 and recursive calls in #2
+'''
