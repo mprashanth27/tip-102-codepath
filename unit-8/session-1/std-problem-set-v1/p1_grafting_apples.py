65aecfd6e95de7dba1ea9c7b36769125c612e2fc
m.prashanth <prashanthmangena@gmail.com>
2025-07-25 12:44:07 -0700

    add U8S1StdV1 my p1 code

--- unit-8/session-1/std-problem-set-v1/p1_grafting_apples.py
+++ unit-8/session-1/std-problem-set-v1/p1_grafting_apples.py
@@ -1,14 +1,42 @@
 '''
-You are grafting different varieties of 
-    apple onto the same root tree can produce many 
-        different varieties of apples! Given the following TreeNode class, create the binary tree depicted below.
-            The text representing each node should should be used as the value.
-                The root, or topmost node in the tree TreeNode("Trunk") has been provided for you.
+Q. You are grafting different varieties of apple onto the same root tree can produce many 
+different varieties of apples! Given the following TreeNode class, create the binary tree depicted below.
+The text representing each node should should be used as the value.
+The root, or topmost node in the tree TreeNode("Trunk") has been provided for you.
 
 Understand: Input root, output: nothing is returned create the tree
 Match: binary tree
 Plan: Create each node and assign lefts and rights
 
+'''
+'''
+U-nderstand: 
+create the binary tree depicted below. text representing each node should be used as the value
+             Trunk
+          /         \
+      Mcintosh   Granny Smith
+      /     \       /     \
+    Fuji   Opal   Crab   Gala
+
+i/p = root: TreeNode("Trunk")
+o/p = No
+
+#Constraints
+
+#Edge cases
+[]
+['Trunk', 'Mcintosh'] # 1 child
+
+M-atch: binary tree
+
+P-lan:
+
+
+I-mplement:
+
+R-eview:
+
+E-valuate:
 '''
 from collections import deque
 
@@ -36,18 +64,22 @@ def print_tree(root):
     print(result)
 
 root = TreeNode("Trunk")
-node1 = TreeNode("Macintosh")
+
+# create nodes
+node1 = TreeNode("Mcintosh")
 node2 = TreeNode("Granny Smith")
 node3 = TreeNode("Fuji")
 node4 = TreeNode("Opal")
 node5 = TreeNode("Crab")
 node6 = TreeNode("Gala")
 
-root.left=node1
-root.right=node2
-node1.left=node3
-node1.right=node4
-node2.left=node5
-node2.right=node6
+# map nodes & create tree
+root.left = node1
+root.right = node2
+node1.left = node3
+node1.right = node4
+node2.left = node5
+node2.right = node6
 
-print_tree(root)
+# Using print_tree() included at the top of this page
+print_tree(root) # 
