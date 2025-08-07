8cb214fc606cfa09ba42de1d930ac0b098cfa035
m.prashanth <prashanthmangena@gmail.com>
2025-08-06 23:05:16 -0700

    add U6S1StdV1 my p1 code

--- unit_6/session_1/std_problem_set_v1/p1_building_a_playlist.py
+++ unit_6/session_1/std_problem_set_v1/p1_building_a_playlist.py
@@ -4,7 +4,11 @@ class SongNode:
 	def __init__(self, song, next=None):
 		self.song = song
 		self.next = next
-		
-top_hits_2010s = SongNode("Uptown Funk", SongNode("Party Rock Anthem", SongNode("Bad Romance")))
 
-print_linked_list(top_hits_2010s)
+song_3 = SongNode("Bad Romance")
+song_2 = SongNode("Party Rock Anthem",song_3)
+song_1 = SongNode("Uptown Funk", song_2)
+
+#top_hits_2010s = SongNode("Uptown Funk", SongNode("Party Rock Anthem", SongNode("Bad Romance")))
+
+print_linked_list(song_1)
