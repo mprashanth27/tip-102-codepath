35d452ac612dc3f1dc3463fd5a7dac5055b35f57
m.prashanth <prashanthmangena@gmail.com>
2025-08-06 17:40:58 -0700

    renames unit-6 folder and makes it a python module by adding init.py to import linked_list functions in other folder's .py files

---
+++ unit_6/linked_list.py
@@ -0,0 +1,7 @@
+# For testing
+def print_linked_list(node):
+    current = node
+    while current:
+        print(current.song, end=" -> " if current.next else "")
+        current = current.next
+    print()
